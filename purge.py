eb94b754b2e7575a954f7ae5280daa654069e200
r3cursive123 <r3cursive@null.net>
2018-04-12 18:15:41 -0400

    Update purge.py

--- purge.py
+++ purge.py
@@ -6,10 +6,10 @@ import os
 if os.geteuid() != 0:
     print('You must run this script as root!!!')
     exit()
-
+os.system("clear")
 print colored("Removing current proxychains.conf" + "\n","red")
 os.system("rm /etc/proxychains.conf")
 print colored("Copying our custom proxychains config file to /etc" + "\n","red")
 shutil.copy("proxychains.conf","/etc/proxychains.conf")
-print colored("Copied!" + "\n" ,"blue")
+print colored("Copied!" + "\n" ,"red")
 print colored("You may now run proxy_scrape.py to get a fresh list" + "\n" ,"yellow")
