1b27429fc512b110f94ffb9072cd349b968dfe6c
r3cursive123 <r3cursive@null.net>
2018-04-12 18:13:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ import os
 if os.geteuid() != 0:
     print('You must run this script as root!!!')
     exit()
-
+os.system("clear")
 print colored("Creating backup of original proxychains.conf" + "\n","red")
 shutil.copy("/etc/proxychains.conf","/etc/proxychains.conf.orig")
-print colored("Backup created as /etc/proxychains.conf.orig" + "\n","blue")
+print colored("Backup created as /etc/proxychains.conf.orig" + "\n","red")
 print colored("Copying our custom proxychains config file to /etc" + "\n","red")
 os.system("rm /etc/proxychains.conf")
 shutil.copy("proxychains.conf","/etc/proxychains.conf")
-print colored("Copied!" + "\n" ,"blue")
+print colored("Copied!" + "\n" ,"red")
 print colored("You may now run proxy_scrape.py" + "\n" ,"yellow")
 
 
